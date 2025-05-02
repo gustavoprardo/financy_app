e4a51e132b47125ada6e41b819ae21c51133e9c6
Gustavo Prardo <130465875+DevBaraka@users.noreply.github.com>
2025-05-02 09:46:14 -0300

    splashpage

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:financy_app/features/slpash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -5,10 +6,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Home')),
-      ),
-    );
+    return MaterialApp(home: SplashPage());
   }
 }
